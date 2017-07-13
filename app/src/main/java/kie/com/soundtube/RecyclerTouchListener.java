daae2dd1e2b86415f51ea01ae338a08c0669cc85
kieChang <kie4280@livemail.tw>
2017-07-13 21:24:03 +0800

    click listener

---
+++ app/src/main/java/kie/com/soundtube/RecyclerTouchListener.java
@@ -0,0 +1,61 @@
+package kie.com.soundtube;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by kieChang on 2017/7/13.
+ */
+
+interface OnItemClicked {
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+}
+
+public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+    OnItemClicked clickListener = null;
+    GestureDetector gestureDetector = null;
+
+    public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final OnItemClicked listener) {
+        clickListener = listener;
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return super.onSingleTapUp(e);
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                    clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View child = rv.findChildViewUnder(e.getX(), e.getY());
+        if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+            clickListener.onLongClick(child, rv.getChildAdapterPosition(child));
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+}
